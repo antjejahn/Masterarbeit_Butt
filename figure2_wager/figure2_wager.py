b42a399f3720e19b9f724f17dada7f18cee824fc
rehan-b <rehan.b@outlook.de>
2024-05-29 18:14:30 +0200

    better modularity

--- figure2_wager/figure2_wager.py
+++ figure2_wager/figure2_wager.py
@@ -7,84 +7,76 @@ from utils import (
     bagging_decision_trees,
     generate_data,
     inf_JK_bagged_variance,
-    step_function,
     simulate_bagging_and_variance,
     save_results_png,
 )
 
 
 def main():
-    """Main function to run the simulation and saving the results as png."""
 
     # Constants
-    MAX_LEAF_NODES = 5
     CHUNK_SIZE = 250
-    sampling_noise_variance = [.25] #[0.25,0.5,round(np.sqrt(2),4), 2.0]
-    min_samples_leaf = 1       # default = 1
     ijk = False
+    fix_x_points = True
     
-    for noise in sampling_noise_variance:
-        sampling_noise = noise
-        
-        # Simulation parameters
-        n_data_points = 500
-        n_simulations = 1_000
-        B = 1000
-        seed = 62
+    # Simulation parameters
+    n = 500
+    n_sim = 1_000
+    B = 1000 # Paper uses 10_000
+    args = {"max_leaf_nodes": 5}
+    seed = 45
+    new_data = np.linspace(0, 1, n)
 
-        # Generate fixed data points for predictions
-        new_data = np.linspace(0, 1, n_data_points)
+    # Arrays to store the predictions and estimated variances
+    bagged_preds = np.zeros((n_sim, new_data.shape[0]))
+    est_vars = np.zeros((n_sim, new_data.shape[0]))
 
-        # Arrays to store the predictions and estimated variances
-        bagged_predictions_all = np.zeros((n_simulations, new_data.shape[0]))
-        est_variances_all = np.zeros((n_simulations, new_data.shape[0]))
+    # Parallelize simulations with progress bar
+    with ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                simulate_bagging_and_variance,
+                n=n,
+                B=B,
+                new_data=new_data,
+                simulation_index=i,
+                seed=seed,
+                dt_args=args,
+                fix_x_points=fix_x_points,
+                ijk_calculation=ijk,
+                chunk_size=CHUNK_SIZE
+            )
+            for i in range(n_sim)
+        ]
 
-        # Parallelize simulations with progress bar
-        with ProcessPoolExecutor() as executor:
-            futures = [
-                executor.submit(
-                    simulate_bagging_and_variance,
-                    new_data=new_data,
-                    n_data_points=n_data_points,
-                    B=B,
-                    simulation_index=i,
-                    seed=seed,
-                    noise_variance_for_y=sampling_noise,
-                    max_leaf_nodes=MAX_LEAF_NODES,
-                    chunk_size=CHUNK_SIZE,
-                    min_samples_leaf=min_samples_leaf,
-                    ijk_calculation=ijk
-                )
-                for i in range(n_simulations)
-            ]
+        for i, future in enumerate(
+            tqdm(futures, desc="Simulations", unit="simulation")
+        ):
+            bagged_prediction, est_variance = future.result()
+            bagged_preds[i, :] = bagged_prediction
+            est_vars[i, :] = est_variance
 
-            for i, future in enumerate(
-                tqdm(futures, desc="Simulations", unit="simulation")
-            ):
-                bagged_predictions, est_variances = future.result()
-                bagged_predictions_all[i, :] = bagged_predictions
-                est_variances_all[i, :] = est_variances
+    # Calculate true variance of bagged predictions
+    true_vars = bagged_preds.var(axis=0, ddof=1)
+    est_vars_mean = est_vars.mean(axis=0)
+    est_vars_std = est_vars.std(axis=0, ddof=1)
 
-        # Calculate true variance of bagged predictions
-        true_variances = bagged_predictions_all.var(axis=0, ddof=1)
-        est_variances_mean = est_variances_all.mean(axis=0)
-        est_variances_std = est_variances_all.std(axis=0, ddof=1)
+    print(f"Mean true variance: {round(np.mean(true_vars), 10)}")
+    print(f"Mean estimated variance: {round(np.mean(est_vars_mean), 10)}")
+    print(f"Min estimated variance: {round(np.min(est_vars), 10)}")
 
-        print(f"Mean true variance: {round(np.mean(true_variances), 10)}")
-        print(f"Mean estimated variance: {round(np.mean(est_variances_mean), 10)}")
-        print(f"Min estimated variance: {round(np.min(est_variances_all), 10)}")
-
-        save_results_png(
-            new_data,
-            true_variances,
-            est_variances_mean,
-            est_variances_std,
-            n_data_points,
-            n_simulations,
-            B,
-            seed,
-            sampling_noise
-        )
+    save_results_png(
+        new_data=new_data,
+        true_variances=true_vars,
+        est_variances_mean=est_vars_mean,
+        est_variances_std=est_vars_std,
+        n_data_points=n,
+        n_simulations=n_sim,
+        B=B,
+        seed=seed,
+        dt_args=args,
+        fixed_x_points=fix_x_points,
+    )
 
 
 if __name__ == "__main__":
--- figure2_wager/utils.py
+++ figure2_wager/utils.py
@@ -1,99 +1,73 @@
 import numpy as np
-from typing import Tuple
+from typing import Tuple, Dict
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 import time
 
-
-def create_bootstrap_indices_and_Nbi(
-    n_data_points: int, B: int, seed: int
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Creates bootstrap indices and counts.
-
-    Parameters:
-    - n_data_points (int): The number of data points.
-    - B (int): The number of bootstrap samples to generate.
-    - seed (int, optional): The seed value for random number generation. Default is None.
-
-    Returns:
-    - indices_list (np.ndarray): An array of shape (B, n_data_points) containing the bootstrap indices.
-    - counts (np.ndarray): An array of shape (B, n_data_points) containing the counts.
-
-    Example:
-    >>> indices_list, counts = create_bootstrap_indices_and_Nbi(5, 3, seed=42)
-    >>> indices_list
-    (array([[3, 4, 2, 4, 4],
-        [1, 2, 2, 2, 4],
-        [3, 2, 4, 1, 3]])
-    >>> counts
-    array([[0, 0, 1, 1, 3],
-        [0, 1, 3, 0, 1],
-        [0, 1, 1, 2, 1]]))
-    """
-    np.random.seed(seed=seed)
-    indices_list = np.random.choice(
-        n_data_points, size=(B, n_data_points), replace=True
-    )
-    counts = np.zeros(shape=(B, n_data_points), dtype=int)
-    for x_i in range(n_data_points):
-        counts[:, x_i] = np.sum(indices_list == x_i, axis=1)
-    return indices_list, counts
-
-
-def bagging_decision_trees(
-    x: np.ndarray, y_noisy: np.ndarray,new_data: np.ndarray, B: int, 
-    max_leaf_nodes: int, seed: int, min_samples_leaf: int= 1,
-) -> Tuple[np.ndarray, np.ndarray]:
-    """Returns: \n
-    tree_predictions_b(B, n_data_points) , \n
-    N_bi(B, n_data_points)"""
-
-    n_data_points = x.shape[0]
-    n_pred_points = new_data.shape[0]
-    tree_predictions_b = np.zeros(shape=(B, n_pred_points))
-    indices_list, N_bi = create_bootstrap_indices_and_Nbi(
-        n_data_points=n_data_points, B=B, seed=seed
-    )
-
-    for b in range(B):
-        tree_model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes,min_samples_leaf=min_samples_leaf)
-        tree_model.fit(
-            X=x[indices_list[b]].reshape(-1, 1), y=y_noisy[indices_list[b]]
-        )
-        tree_predictions_b[b, :] = tree_model.predict(X=new_data.reshape(-1, 1))
-
-    return tree_predictions_b, N_bi
-
-
-def step_function(x):
-    """
-    Returns the step function value for the given input.
-
-    Parameters:
-    x (float or array-like): The input value(s) for which the step function value is calculated.
-
-    Returns:
-    float or array-like: The step function value(s) corresponding to the input value(s).
-    """
-    return np.where(
-        x < 0.35,
-        0.0,
-        np.where(x < 0.45, 0.7, np.where(x < 0.55, 1.4, np.where(x < 0.65, 0.7, 0.0))),
-    )
-
-
-def generate_data(
-    n_data_points: int, seed: int, noise_variance=0.25
+def save_results_png(
+    new_data: np.ndarray,
+    true_variances: np.ndarray,
+    est_variances_mean: np.ndarray,
+    est_variances_std: np.ndarray,
+    n_data_points: np.ndarray,
+    n_simulations: int,
+    B: int,
+    seed: int,
+    dt_args: Dict,
+    fixed_x_points: bool,
 ):
-    """Generates noisy data based on the true function."""
-    np.random.seed(seed=seed)
-    x = np.random.uniform(0, 1, n_data_points)
-    #x = np.linspace(0, 1, n_data_points)
-    y_true = step_function(x)
-    noise = np.random.normal(loc=0, scale=np.sqrt(noise_variance), size=n_data_points)
-    return x , y_true, y_true + noise
+    """
+    Save the results plot as a PNG file.
 
+    Args:
+        new_data (array-like): The x-axis values to predict.
+        true_variances (array-like): The true variances.
+        est_variances_mean (array-like): The mean estimated variances.
+        est_variances_std (array-like): The standard deviation of estimated variances.
+        n_data_points (int): The number of data points.
+        n_simulations (int): The number of simulations.
+        B (int): The bootstrap value.
+        seed (int): The seed value.
+
+    Returns:
+        None
+    """
+
+    # Plotting the results
+    plt.figure(figsize=(10, 6))
+    plt.plot(new_data, true_variances, label="True Variance")
+    plt.plot(new_data, est_variances_mean, label="Mean Est. Variance")
+    plt.fill_between(
+        new_data,
+        est_variances_mean - est_variances_std,
+        est_variances_mean + est_variances_std,
+        color="b",
+        alpha=0.2,
+        label="±1 std",
+    )
+    plt.title("True Variance of Bagged Predictions Across Simulated Datasets")
+    plt.xlabel("x")
+    plt.ylabel("Variance")
+    plt.ylim(-0.01, 0.06)
+    plt.grid(True)  
+
+    plt.text(
+        0.05,
+        0.04,
+        f"data_points = {n_data_points}\nsimulations = {n_simulations}\nbootstrap(B) = {B}\nfixed_x={fixed_x_points}",
+        fontsize=12,
+        bbox=dict(facecolor="white", alpha=0.5),
+    )
+    plt.legend()
+    if fixed_x_points:
+        plt.savefig(
+            f"figure2_wager/figures/figure2_seed{seed}_nB{B}_fixed_x_{dt_args.items()}.png"
+        )
+    else:
+        plt.savefig(
+            f"figure2_wager/figures/figure2_seed{seed}_nB{B}_{dt_args.items()}.png"
+        )
+    #{int(time.time())}
 
 def inf_JK_bagged_variance(
     N_bi: np.ndarray, T_N_b: np.ndarray, chunk_size: int
@@ -139,7 +113,6 @@ def inf_JK_bagged_variance(
     var_inf_JK_U = cov_vector - bias_correction
     return var_inf_JK_U
 
-
 def inf_JK_bagged_variance_simple(N_bi: np.ndarray, T_N_b: np.ndarray) -> np.ndarray:
     """
     Calculate the Jackknife-Infenitesimal Variance for bagged learners.
@@ -177,42 +150,150 @@ def inf_JK_bagged_variance_simple(N_bi: np.ndarray, T_N_b: np.ndarray) -> np.nda
     var_inf_JK_U = cov_vector - bias_correction
     return var_inf_JK_U
 
-
-def simulate_bagging_and_variance(
-    new_data: np.ndarray,
-    n_data_points: int,
-    B: int,
-    simulation_index: int,
-    seed: int,
-    noise_variance_for_y=0.25,
-    max_leaf_nodes=5,
-    chunk_size=250,
-    ijk_calculation=False,
-    min_samples_leaf: int= 1,
+def create_bootstrap_indices_and_Nbi(
+    n: int, B: int, seed: int=None
 ) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Simulate bagging and calculate variance for a single run.
+    Creates bootstrap indices and counts.
 
     Parameters:
-        x_points (array-like): The x-coordinates of the data points.
-        y_true (array-like): The true y-values corresponding to the x-coordinates.
-        B (int): The number of bootstrap samples to generate.
-        simulation_index (int): The index of the simulation.
-        seed (int): The seed value for random number generation.
-        noise_variance_for_y (float, optional): The variance of the noise added to y-values. Defaults to 0.25.
-        max_leaf_nodes (int, optional): The maximum number of leaf nodes in the decision trees. Defaults to 5.
-        chunk_size (int, optional): The size of chunks used for infinitesimal jackknife variance calculation. Defaults to 250.
+    - n (int): The number of observations.
+    - B (int): The number of bootstrap samples to generate.
+    - seed (int, optional): The seed value for random number generation. Default is None.
 
     Returns:
-        tuple: A tuple containing the bagged predictions and estimated variances.
+    - boot_indices (np.ndarray): An array of shape (B, n_data_points) containing the bootstrap indices.
+    - boot_counts  (np.ndarray): An array of shape (B, n_data_points) containing the counts.
 
+    Example:
+    >>> indices_list, counts = create_bootstrap_indices_and_Nbi(5, 3, seed=42)
+    >>> indices_list
+    array([[3, 4, 2, 4, 4],
+           [1, 2, 2, 2, 4],
+           [3, 2, 4, 1, 3]])
+    >>> counts
+    array([[0, 0, 1, 1, 3],
+           [0, 1, 3, 0, 1],
+           [0, 1, 1, 2, 1]])
+    """
+    rng = np.random.default_rng(seed)
+    boot_indices = rng.integers(0, n, size=(B, n))
+    boot_counts = np.apply_along_axis(lambda x: np.bincount(x, minlength=n), axis=1, arr=boot_indices)
+    return boot_indices, boot_counts
+
+def generate_data(
+    n: int, seed: int=None, noise_variance=0.25, fix_points=False
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generate synthetic data for a regression problem.
+
+    Args:
+        n (int): The number of data points to generate.
+        seed (int, optional): The seed value for the random number generator. Defaults to None.
+        noise_variance (float, optional): The variance of the Gaussian noise added to the true labels. Defaults to 0.25.
+        fix_points (bool, optional): If True, fix the x-coordinates of the data points. Defaults to False.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]: A tuple containing three arrays: \n
+            - x: The x-coordinates of the data points. \n
+            - y true: The true labels without noise. \n
+            - y noisy: The true labels with added Gaussian noise.
     """
 
-    x, y_true, y_noisy = generate_data(
-        n_data_points=n_data_points,
-        seed=seed + simulation_index,
-        noise_variance=noise_variance_for_y,
+    rng = np.random.default_rng(seed)
+    x = np.linspace(0, 1, n) if fix_points else rng.uniform(0, 1, n)
+    
+    # Step function
+    y_true = np.piecewise(
+        x,
+        [x < 0.35, (x >= 0.35) & (x < 0.45), (x >= 0.45) & (x < 0.55), (x >= 0.55) & (x < 0.65), x >= 0.65],
+        [0.0, 0.7, 1.4, 0.7, 0.0]
     )
+    
+    noise = rng.normal(loc=0, scale=np.sqrt(noise_variance), size=n)
+    y_noisy = y_true + noise
+    
+    return x, y_true, y_noisy
+
+def bagging_decision_trees(
+    x: np.ndarray, y_noisy: np.ndarray, new_data: np.ndarray, B: int, 
+    dt_args: Dict, seed: int=None
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Train and predict using bagging with decision trees.
+
+    Args:
+        x (np.ndarray): Training data features.
+        y_noisy (np.ndarray): Training data targets with noise.
+        new_data (np.ndarray): New data points to predict.
+        B (int): Number of bootstrap samples and trees.
+        dt_args (dict): Arguments for the DecisionTreeRegressor.
+        seed (int, optional): Random seed for reproducibility. Defaults to None.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: A tuple containing: \n
+            - tree predictions b (np.ndarray): Predictions from each tree of shape=(B, n pred). \n
+            - N bi (np.ndarray): Bootstrap sample counts of shape=(B, n).
+    """
+    n = x.shape[0]
+    n_pred = new_data.shape[0]
+    tree_predictions_b = np.zeros(shape=(B, n_pred))
+    indices_list, N_bi = create_bootstrap_indices_and_Nbi(
+        n=n, B=B, seed=seed
+    )
+    
+    x_reshaped = x.reshape(-1, 1)
+    new_data_reshaped = new_data.reshape(-1, 1)
+
+    for b in range(B):
+        tree_model = DecisionTreeRegressor(**dt_args)
+        tree_model.fit(x_reshaped[indices_list[b]], y_noisy[indices_list[b]])
+        tree_predictions_b[b] = tree_model.predict(new_data_reshaped)
+
+    return tree_predictions_b, N_bi
+
+def simulate_bagging_and_variance(
+    n: int,
+    B: int,
+    new_data: np.ndarray,
+    simulation_index: int,
+    seed: int,
+    dt_args: Dict,
+    noise_var_for_generating_data=0.25,
+    fix_x_points=False,
+    ijk_calculation=False,
+    chunk_size=250
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Simulates bagging and calculates variance for a given set of parameters.
+
+    Args:
+        n (int): Number of data points.
+        B (int): Number of bootstrap samples.
+        new_data (np.ndarray): New data points for prediction.
+        simulation_index (int): Index of the simulation.
+        seed (int): Seed for random number generation.
+        dt_args (Dict): Dictionary of arguments for decision tree.
+        noise_var_for_generating_data (float, optional): Variance of noise for generating data. Defaults to 0.25.
+        fix_x_points (bool, optional): Whether to fix x points. Defaults to False.
+        ijk_calculation (bool, optional): Whether to calculate infinitesimal jackknife variance. Defaults to False.
+        chunk_size (int, optional): Chunk size for variance calculation. Defaults to 250.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: Tuple containing bagged predictions and estimated variances.
+    """
+
+    adjusted_seed = seed + simulation_index
+    
+    x, y_true, y_noisy = generate_data(
+        n=n,
+        seed=adjusted_seed,
+        noise_variance=noise_var_for_generating_data,
+        fix_points=fix_x_points,
+    )
+    
+    if fix_x_points:
+        x = np.linspace(0, 1, n)
 
     # Perform bagging
     tree_predictions_b, N_bi = bagging_decision_trees(
@@ -220,77 +301,15 @@ def simulate_bagging_and_variance(
         y_noisy=y_noisy,
         new_data=new_data,
         B=B,
-        max_leaf_nodes=max_leaf_nodes,
-        seed=seed + simulation_index,
-        min_samples_leaf=min_samples_leaf,
+        dt_args=dt_args,
+        seed=adjusted_seed
     )
     bagged_predictions = tree_predictions_b.mean(axis=0)
 
     if ijk_calculation:
-        # Calculate infinitesimal jackknife variance
         est_variances = inf_JK_bagged_variance(
             N_bi=N_bi, T_N_b=tree_predictions_b, chunk_size=chunk_size
         )
-    else: est_variances = np.zeros(n_data_points)
+    else: est_variances = np.zeros(n)
 
     return bagged_predictions, est_variances
-
-
-def save_results_png(
-    new_data: np.ndarray,
-    true_variances: np.ndarray,
-    est_variances_mean: np.ndarray,
-    est_variances_std: np.ndarray,
-    n_data_points: np.ndarray,
-    n_simulations: int,
-    B: int,
-    seed: int,
-    noise_variance 
-):
-    """
-    Save the results plot as a PNG file.
-
-    Args:
-        new_data (array-like): The x-axis values to predict.
-        true_variances (array-like): The true variances.
-        est_variances_mean (array-like): The mean estimated variances.
-        est_variances_std (array-like): The standard deviation of estimated variances.
-        n_data_points (int): The number of data points.
-        n_simulations (int): The number of simulations.
-        B (int): The bootstrap value.
-        seed (int): The seed value.
-
-    Returns:
-        None
-    """
-
-    # Plotting the results
-    plt.figure(figsize=(10, 6))
-    plt.plot(new_data, true_variances, label="True Variance")
-    plt.plot(new_data, est_variances_mean, label="Mean Est. Variance")
-    plt.fill_between(
-        new_data,
-        est_variances_mean - est_variances_std,
-        est_variances_mean + est_variances_std,
-        color="b",
-        alpha=0.2,
-        label="±1 std",
-    )
-    plt.title("True Variance of Bagged Predictions Across Simulated Datasets")
-    plt.xlabel("x")
-    plt.ylabel("Variance")
-    #plt.ylim(-0.02, 0.25)
-    plt.grid(True)  
-
-    plt.text(
-        0.05,
-        0.05,
-        f"data_points = {n_data_points}\nsimulations = {n_simulations}\nbootstrap(B) = {B}",
-        fontsize=12,
-        bbox=dict(facecolor="white", alpha=0.5),
-    )
-    plt.legend()
-    plt.savefig(
-        f"figure2_wager/figures/figure2_nB{B}_nsim{n_simulations}_noise{noise_variance}.png"
-    )
-    #{int(time.time())}
