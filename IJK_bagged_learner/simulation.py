09304aa86ae5c3ec5449d1e058d04d5c138bb012
rehan-b <rehan.b@outlook.de>
2024-06-02 10:41:22 +0200

    version 3

---
+++ IJK_bagged_learner/simulation.py
@@ -0,0 +1,81 @@
+import time
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
+import pandas as pd
+from utils import simulate_bagging_and_variance, save_results_png, save_result_csv
+
+
+def main():
+    for fix in [True, False]:
+
+        ############## Constants  ################################
+        ijk = True
+        fix_x_points = fix
+        weights = None
+        folder_name = "wager_figure2"
+
+        ####### Simulation parameters  ############################
+        n = 500
+        n_sim = 1_00
+        B = 10_0  # Paper uses 10_000
+        args = {"max_leaf_nodes": 5}
+        seed = 45
+        new_data = np.linspace(0, 1, 500)
+        ###########################################################
+
+        bagged_preds = np.zeros((n_sim, new_data.shape[0]))
+        est_vars = np.zeros((n_sim, new_data.shape[0]))
+
+        with ProcessPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    simulate_bagging_and_variance,
+                    n=n,
+                    B=B,
+                    new_data=new_data,
+                    simulation_index=i,
+                    seed=seed,
+                    dt_args=args,
+                    fix_x_points=fix_x_points,
+                    ijk_calculation=ijk,
+                    weights=weights,
+                )
+                for i in range(n_sim)
+            ]
+
+            for i, future in enumerate(
+                tqdm(futures, desc="Simulations", unit="simulation")
+            ):
+                bagged_prediction, est_variance = future.result()
+                bagged_preds[i, :] = bagged_prediction
+                est_vars[i, :] = est_variance
+
+        save_result_csv(
+            fix_x_points=fix_x_points,
+            seed=seed,
+            B=B,
+            args=args,
+            bagged_preds=bagged_preds,
+            est_vars=est_vars,
+            new_data=new_data,
+            folder_name=folder_name,
+        )
+
+        save_results_png(
+            new_data=new_data,
+            bagged_preds=bagged_preds,
+            est_vars=est_vars,
+            n_data_points=n,
+            B=B,
+            seed=seed,
+            dt_args=args,
+            fixed_x_points=fix_x_points,
+            folder_name=folder_name,
+        )
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- runtime: %s minutes ---" % round((time.time() - start_time) / 60, 2))
