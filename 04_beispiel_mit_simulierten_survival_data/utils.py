4c59ad3c803c016435fbe98fb8b835c3083ffe09
rehan-b <rehan.b@outlook.de>
2024-07-30 13:00:08 +0200

    ..

--- 04_beispiel_mit_simulierten_survival_data/utils.py
+++ 04_beispiel_mit_simulierten_survival_data/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from lifelines import KaplanMeierFitter
 
 ## Funktion zur Erstellung der simulierten Daten
 def create_surv_data(shape_weibull=1.5, scale_weibull_base=50, rate_censoring=0.02, n=1000, 
@@ -55,15 +56,40 @@ def create_surv_data(shape_weibull=1.5, scale_weibull_base=50, rate_censoring=0.
     return pd.DataFrame(data)
 
 
-## Funktion zur Erstellung des neuen Datensatzes in abhängigkeit eines zeitpunktes t
-def create_new_dataset(data, t):
+
+def create_new_dataset_with_ipcw_weights(data: pd.DataFrame, t: np.float64) -> pd.DataFrame:
     new_data = data.copy()
 
-    new_data.loc[(data['t'] <= t) & (data['event'] == 1), 'survived'] = int(0)
-    new_data.loc[(data['t'] >= t) & (data['event'] == 0), 'survived'] = int(1)
-    new_data.loc[(data['t'] >= t) & (data['event'] == 1), 'survived'] = int(1)
-    new_data.loc[(data['t'] <= t) & (data['event'] == 0), 'survived'] = int(999)
+    new_data.loc[(data['time'] <= t) & (data['event'] == 1), 'survived'] = int(0)
+    new_data.loc[(data['time'] >= t) & (data['event'] == 0), 'survived'] = int(1)
+    new_data.loc[(data['time'] >= t) & (data['event'] == 1), 'survived'] = int(1)
+    new_data.loc[(data['time'] <= t) & (data['event'] == 0), 'survived'] = int(999)
 
     new_data['survived'] = new_data['survived'].astype(int)
 
+    kmf = KaplanMeierFitter()
+    kmf.fit(new_data['time'], event_observed=1-new_data['event'])
+    ipcw_weights =  1/kmf.survival_function_at_times(new_data['time'])
+    ipcw_weight_tau = 1/kmf.survival_function_at_times(t)
+    new_data['weights_ipcw'] = np.where(new_data['survived']==1, ipcw_weight_tau, np.where(new_data['survived']==0, ipcw_weights, 0))
+
     return pd.DataFrame(new_data)
+
+def train_test_split_into_df(df_train, df_test, y_train, y_test):
+
+    df_train.reset_index(drop=True, inplace=True)
+    df_test.reset_index(drop=True, inplace=True)
+
+    y_train_df = pd.DataFrame({'event': [event for event, time in y_train], 'time': [time for event, time in y_train]})
+    y_test_df = pd.DataFrame({'event': [event for event, time in y_test], 'time': [time for event, time in y_test]})
+
+    df_train.reset_index(drop=True, inplace=True)
+    df_test.reset_index(drop=True, inplace=True)
+
+    df_train['event'] = y_train_df['event']
+    df_train['time'] = y_train_df['time']
+    df_test['event'] = y_test_df['event']
+    df_test['time'] = y_test_df['time']
+
+    return df_train, df_test
+
