4fef31184db197b0c280253f260be6e67fc9d584
rehan-b <rehan.b@outlook.de>
2024-07-31 13:30:55 +0200

    stand_passt soweit_ nun simulieren im naechsten vorgang

--- 04_beispiel_mit_simulierten_survival_data/utils.py
+++ 04_beispiel_mit_simulierten_survival_data/utils.py
@@ -102,26 +102,16 @@ def get_Nbi(lists):
     return counts
 
 def inf_JK_bagged_variance(
-    N_bi: np.ndarray, T_N_b: np.ndarray, weights: np.ndarray = None, m: int = None
+    N_bi: np.ndarray, T_N_b: np.ndarray, weights: np.ndarray = None
 ):
     B, n = N_bi.shape
     T_N_b_mean = np.mean(T_N_b, axis=0)
+    m = np.count_nonzero(weights)
 
-    if weights is None:
+    cov_i = ((N_bi - n * weights[0]).T @ (T_N_b - T_N_b_mean)) / B
+    cov_i_hoch2 = cov_i**2
+    biased_var_estimate = np.sum(cov_i_hoch2, axis=0)
 
-        cov_i = ((N_bi - 1).T @ (T_N_b - T_N_b_mean)) / B
-        cov_i_hoch2 = cov_i**2
-        biased_var_estimate = np.sum(cov_i_hoch2, axis=0)
+    bias_correction = n / B * (m - 1) / m * np.var(T_N_b, axis=0)
 
-        bias_correction = ((n - 1) / B) * np.var(T_N_b, axis=0)
-        return biased_var_estimate, bias_correction
-
-    else:
-
-        cov_i = ((N_bi - n * weights[0]).T @ (T_N_b - T_N_b_mean)) / B
-        cov_i_hoch2 = cov_i**2
-        biased_var_estimate = np.sum(cov_i_hoch2, axis=0)
-
-        bias_correction = n / B * (m - 1) / m * np.var(T_N_b, axis=0)
-
-        return biased_var_estimate, bias_correction
+    return biased_var_estimate, bias_correction
