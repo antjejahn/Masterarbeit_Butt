5728f46905360ec9981ccb648ed0f2b0f5aaa7de
rehan-b <rehan.b@outlook.de>
2024-07-31 11:20:28 +0200

    ..

--- 04_beispiel_mit_simulierten_survival_data/utils.py
+++ 04_beispiel_mit_simulierten_survival_data/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from lifelines import KaplanMeierFitter
+from sklearn.ensemble import RandomForestClassifier
 
 ## Funktion zur Erstellung der simulierten Daten
 def create_surv_data(shape_weibull=1.5, scale_weibull_base=50, rate_censoring=0.02, n=1000, 
@@ -93,4 +94,37 @@ def train_test_split_into_df(df_train: pd.DataFrame, df_test: pd.DataFrame, y_tr
     df_test['time'] = y_test_df['time']
 
     return df_train, df_test
+
+class BootstrapRandomForestClassifier(RandomForestClassifier):
+    def __init__(self, n_estimators=100, criterion="gini", max_depth=None,
+                 min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.,
+                 max_features="auto", max_leaf_nodes=None, min_impurity_decrease=0.,
+                 bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0,
+                 warm_start=False, class_weight=None, ccp_alpha=0.0, max_samples=None):
+        super().__init__(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth,
+                         min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf,
+                         min_weight_fraction_leaf=min_weight_fraction_leaf, max_features=max_features,
+                         max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease,
+                         bootstrap=bootstrap, oob_score=oob_score, n_jobs=n_jobs, random_state=random_state,
+                         verbose=verbose, warm_start=warm_start, class_weight=class_weight, ccp_alpha=ccp_alpha,
+                         max_samples=max_samples)
+        self.tree_predictions_ = []
+
+    def fit(self, X, y, sample_weight=None):
+        if isinstance(X, pd.DataFrame):
+            self.feature_names_ = X.columns
+            X = X.values
+        if isinstance(y, pd.Series):
+            y = y.values
+
+        super().fit(X, y, sample_weight)
+        self._store_tree_predictions(X)
+        return self
+
+    def _store_tree_predictions(self, X):
+        for estimator in self.estimators_:
+            self.tree_predictions_.append(estimator.predict_proba(X)[:, 1])
+
+    def get_bootstrap_samples_and_predictions(self):
+        return [(indices, predictions) for indices, predictions in zip(self.estimators_samples_, self.tree_predictions_)]
 
